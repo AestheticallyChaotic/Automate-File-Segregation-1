e46df48aef0234e45b8354ffc578861884ecc3c8
AestheticallyChaotic <76975708+AestheticallyChaotic@users.noreply.github.com>
2022-12-02 18:12:03 -0800

    Add files via upload

---
+++ Project 111/Move_File.py
@@ -0,0 +1,8 @@
+import os
+import shutil
+
+from_dir = "C:/Users/under/Desktop/Python/Project 111/Downloads"
+to_dir = "C:/Users/under/Desktop/Python/Project 111/Document_Files"
+
+list_of_files = os.listdir(from_dir)
+print(list_of_files)
